d7276a7ead9efc499137237b07adcac5b6b48c86
Alper Kaya <20200808002@ogr.akdeniz.edu.tr>
2023-04-19 16:23:49 +0300

    Delete pricing from footer

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="/consumer/landingPage.php" class="nav-link px-2 text-body-secondary">Home</a></li>
         <li class="nav-item"><a href="/consumer/features.php" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="/consumer/pricing.php" class="nav-link px-2 text-body-secondary">Pricing</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
         <li class="nav-item"><a href="/consumer/features.php" class="nav-link px-2 text-body-secondary">About</a></li>
         </ul>
--- toast.php
+++ toast.php
@@ -1,7 +1,7 @@
 <div class="toast-container position-fixed bottom-0 end-0 p-3">
     <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
-        <img src="./images/help.png" class="rounded me-2" alt="..." height="20px"><br>
+        <img src="/consumer/images/help.png" class="rounded me-2" alt="..." height="20px"><br>
         <strong class="me-auto">Do you need any help?</strong>
         <small>10 seconds ago</small>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
