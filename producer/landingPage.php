086fc29622f77dd9238285585e853a3eb7d81aa0
Alper Kaya <20200808002@ogr.akdeniz.edu.tr>
2023-04-23 16:51:20 +0300

    Fix Producer Favicons

--- producer/landingPage.php
+++ producer/landingPage.php
@@ -10,7 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <title>Landing Page</title>
 
-    <link rel="icon" type="image/x-icon" href="./images/logo.png"></head>
+    <link rel="icon" type="image/x-icon" href="../consumer/images/logo.png"></head>
 <body>
     <?php
         /*
